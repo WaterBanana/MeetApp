0882394cd22a1ba2f85af9c93705863e1fd60908
scarfyeatsduck <eaterofduck@gmail.com>
2015-07-08 18:42:14 -0700

    AVAILABILITY SCREEN ITS NOT FINISHED OKAY FINE BYE

---
+++ app/src/main/java/com/waterbanana/meetapp/Availability.java
@@ -0,0 +1,31 @@
+package com.waterbanana.meetapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Availability extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView( R.layout.activity_availability );
+    }
+
+    /*
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate( R.layout.activity_availability, container, false );
+
+        return view;
+    }
+    */
+
+
+
+}
